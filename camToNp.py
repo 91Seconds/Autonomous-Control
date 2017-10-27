cc15b6b6b1ea2dd81e99aad8239d224d49edcf77
91Seconds <king.ciaran72@gmail.com>
2017-10-27 05:44:57 +0000

    woops, merge

--- camToNp.py
+++ camToNp.py
@@ -4,7 +4,7 @@ import picamera
 
 
 
-camera = picamera.Picamera()
+camera = picamera.PiCamera()
 camera.resolution = (320,240)
 output = np.empty((240,320,3), dtype=np.uint8)
 camera.capture(output, 'rgb')
