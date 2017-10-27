c9d4bcf333a742fedf1a07350141a6359c98444f
91Seconds <king.ciaran72@gmail.com>
2017-10-27 07:43:51 +0000

    flask serving and image with GET at url

--- webDisplay/app.py
+++ webDisplay/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/img')
+def getImg():
+    return send_file("../img/capture1.png", mimetype="image/png")
 
 if __name__ == "__main__":
     app.run(debug=True)
